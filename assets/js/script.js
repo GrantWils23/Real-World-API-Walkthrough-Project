e94a00d4274462991770e2f451953d4e59062adc
Grant <GrantWils.23@googlemail.com>
2022-02-06 19:42:02 +0000

    add the subtle error rectification code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,29 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form
+}
 
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
     
+    // for (entry of form.entries()) { // Test code to check the the data we are sending
+    //     console.log(entry)          // the api is correct.
+    // }
+
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
